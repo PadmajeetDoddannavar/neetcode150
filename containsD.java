13124117a43322bbc0cecd481a57516def6948f7
Padmajeet Doddannavar <155511580+PadmajeetDoddannavar@users.noreply.github.com>
2025-04-12 10:55:22 +0530

    Update containsD.java

--- containsD.java
+++ containsD.java
@@ -1,1 +1,16 @@
-hii jjjj
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int i=0; i< nums.length; i++){
+            if(set.contains(nums[i])){
+                return true;
+            }
+            else{
+                set.add(nums[i]);
+            }
+        }
+        return false;
+    }
+}
